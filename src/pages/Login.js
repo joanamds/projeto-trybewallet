31b453d629c0a7760ed5dc5d5aba2b1d8b5c8e9b
Joana <joanamdsantos1@gmail.com>
2022-10-31 18:10:54 -0300

    criando os campos de input e o botão na página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-page">
+        <h1>Login</h1>
+        <label htmlFor="email">
+          Email
+          <input type="email" name="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input type="password" name="password" data-testid="password-input" />
+        </label>
+        <button type="submit">Entrar</button>
+      </div>
+    );
   }
 }
 
