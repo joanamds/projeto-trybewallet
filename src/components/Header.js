b5c63a5052c39e09e12acc41a7ab715163cd9c6e
Joana <joanamdsantos1@gmail.com>
2022-10-31 19:24:21 -0300

    2. Crie um header para a página de carteira

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      total: 0,
+    };
+  }
+
   render() {
+    const { email } = this.props;
+    const { total } = this.state;
     return (
-      <div>Header</div>
+      <>
+        <p data-testid="email-field">
+          { email }
+        </p>
+        <p data-testid="total-field">
+          { total }
+        </p>
+        <p data-testid="header-currency-field">
+          BRL
+        </p>
+      </>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
