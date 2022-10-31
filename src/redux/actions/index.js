d71fe5267c02510d5f6fc62fd9f315b9d68350a3
Joana <joanamdsantos1@gmail.com>
2022-10-31 16:01:01 -0300

    criando as actions e os reducers

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,16 @@
 // Coloque aqui suas actions
+// actions para o login
+// actions para o wallet
+
+export const USER_FORM = 'USER_FORM';
+export const WALLET_FORM = 'WALLET_FORM';
+
+export const userForm = (payload) => ({
+  type: USER_FORM,
+  payload,
+});
+
+export const walletForm = (payload) => ({
+  type: WALLET_FORM,
+  payload,
+});
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,26 @@
+import { WALLET_FORM } from '../actions';
+
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_FORM:
+    return {
+      ...state,
+      currencies: action.payload.currencies,
+      expenses: action.payload.expenses,
+      editor: action.payload.editor,
+      idToEdit: action.payload.idToEdit,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
