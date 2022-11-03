7edecfd789dc12970510e91a33e1a15d5f94dede
Joana <joanamdsantos1@gmail.com>
2022-11-03 15:21:49 -0300

    7. Implemente a lógica para que a tabela seja alimentada pelo estado da aplicação

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const expensesArray = [...expenses];
     const tableHeaders = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
       'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
     return (
@@ -10,16 +14,80 @@ class Table extends Component {
         <tr>
           { tableHeaders.map((header) => <th key={ header }>{ header }</th>) }
         </tr>
-        {/* através da lista salva de gastos, fazer um map desta lista.
-        cada elemento tera um tr como no cabeçalho englobando os td que
-        terão cada elemento extraído com a ordem do cabeçalho
-        obs: terei que fazer um find nesse map para extrair a moeda de conversão que foi utilizada no gasto
-        obs2: terá que ser extraído o "name" e não o "code" das moedas */}
+        {expensesArray.length > 0
+            && expensesArray.map((expense) => {
+              const { exchangeRates } = expense;
+              const getValues = Object.values(exchangeRates);
+              const getCurr = getValues.find((value) => value.code === expense.currency);
+              const currencyValue = Number(getCurr.ask);
+              const numberValue = Number(expense.value);
+              const value = numberValue * currencyValue;
+              return (
+                <tbody key={ expense.id }>
+                  <tr key={ expense.id }>
+                    <td>
+                      { expense.description }
+                    </td>
+                    <td>
+                      { expense.tag }
+                    </td>
+                    <td>
+                      { expense.method }
+                    </td>
+                    <td>
+                      { numberValue.toFixed(2) }
+                    </td>
+                    <td>
+                      { getCurrency.name }
+                    </td>
+                    <td>
+                      { currencyValue.toFixed(2) }
+                    </td>
+                    <td>
+                      { value.toFixed(2) }
+                    </td>
+                    <td>
+                      Real
+                    </td>
+                    <td>
+                      <button type="button" data-testid="edit-btn">Editar</button>
+                      <button type="button" data-testid="delete-btn">Excluir</button>
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })}
       </table>
     );
   }
 }
 
-// fazer o mapStateToProps para pegar wallet.expenses do estado global da aplicação
-// importar o connect para englobar o componente com o mapStateToProps
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    value: PropTypes.string,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    method: PropTypes.string,
+    tag: PropTypes.string,
+    exchangeRates: PropTypes.shape({
+      ask: PropTypes.string,
+      bid: PropTypes.string,
+      code: PropTypes.string,
+      codein: PropTypes.string,
+      create_date: PropTypes.string,
+      high: PropTypes.string,
+      low: PropTypes.string,
+      name: PropTypes.string,
+      pctChange: PropTypes.string,
+      timestamp: PropTypes.string,
+      varBid: PropTypes.string,
+    }),
+  })).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
