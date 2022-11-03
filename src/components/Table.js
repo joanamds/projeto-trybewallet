258443ffa28b94d9c6ec5e1d7d418582a9d380a5
Joana <joanamdsantos1@gmail.com>
2022-11-03 12:44:37 -0300

    adicionando comentários para organizar o que será trabalhado no próximo requisito

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 
 class Table extends Component {
   render() {
+    const tableHeaders = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
+      'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
     return (
       <table className="table-content">
         Table
         <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
+          { tableHeaders.map((header) => <th key={ header }>{ header }</th>) }
         </tr>
+        {/* através da lista salva de gastos, fazer um map desta lista.
+        cada elemento tera um tr como no cabeçalho englobando os td que
+        terão cada elemento extraído com a ordem do cabeçalho
+        obs: terei que fazer um find nesse map para extrair a moeda de conversão que foi utilizada no gasto
+        obs2: terá que ser extraído o "name" e não o "code" das moedas */}
       </table>
     );
   }
 }
 
+// fazer o mapStateToProps para pegar wallet.expenses do estado global da aplicação
+// importar o connect para englobar o componente com o mapStateToProps
 export default Table;
